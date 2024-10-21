bb8885e47eebd40bed5814e3fea15c7b5a47f9a5
ramaps <stefanuttiramiro@gmail.com>
2024-10-20 21:29:16 -0300

    primer commit

---
+++ js/appMusica.js
@@ -0,0 +1,80 @@
+const tituloCancion = document.querySelector('.reproductor-musica h1');
+const nombreArtista = document.querySelector('.reproductor-musica p');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
+
+const inconoControl = document.getElementById('iconoControl');
+const botonReproducirPausar = document.querySelector('.controles button.boton-reproducir-pausar');
+
+const botonAtras = document.querySelector('.controles button.atras');
+const botonAdelante = document.querySelector('.controles button.adelante');
+
+const canciones = [
+    {
+        titulo:'Angels',
+        nombre:'Robbie Williams',
+        fuente:'music/Angels - Robbie Williams.mp3'
+    },
+    //agregar las canciones que sean necesarias
+]; 
+let indiceCancionActual = 0;
+
+function actualizarInfoCancion(){
+    tituloCancion.textContent = canciones[indiceCancionActual].titulo;
+    nombreArtista.textContent = canciones[indiceCancionActual].nombre;
+    cancion.src = canciones[indiceCancionActual].fuente;
+    cancion.addEventListener('loadeddata',function(){});
+};
+
+cancion.addEventListener('loadedmetadata', function(){
+    progreso.max = cancion.duration;
+    progreso.value = cancion.currentTime;
+});
+
+botonReproducirPausar.addEventListener('click', reproducirPausar);
+
+function reproducirPausar(){
+    if(cancion.paused){
+        reproducirCancion();
+    } else {
+        pausarCancion();
+    }
+};
+
+function reproducirCancion(){
+    cancion.play();
+    inconoControl.classList.add('bi-pause-fill')
+    inconoControl.classList.remove('bi-play-fill')
+}
+
+function pausarCancion(){
+    cancion.pause();
+    inconoControl.classList.remove('bi-pause-fill')
+    inconoControl.classList.add('bi-play-fill')
+}
+
+cancion.addEventListener('timeupdate', function(){
+    if(!cancion.paused){
+        progreso.value = cancion.currentTime;
+    }
+});
+
+progreso.addEventListener('input', function(){
+    cancion.currentTime = progreso.value;
+});
+
+
+botonAdelante.addEventListener('click', function(){
+    indiceCancionActual = (indiceCancionActual + 1) % canciones.length;
+    actualizarInfoCancion();
+    reproducirCancion();
+});
+
+botonAtras.addEventListener('click', function(){
+    indiceCancionActual = (indiceCancionActual - 1 + canciones.length) % canciones.length;
+    actualizarInfoCancion();
+    reproducirCancion();
+});
+
+actualizarInfoCancion();
