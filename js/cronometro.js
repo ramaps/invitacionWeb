11f06dcf8a2f8816c951c2b7072f927dcfc41d31
ramaps <stefanuttiramiro@gmail.com>
2024-10-21 22:35:33 -0300

    agregamos cambios

--- js/cronometro.js
+++ js/cronometro.js
@@ -4,7 +4,7 @@ var minutos = document.getElementById('minutos');
 var segundos = document.getElementById('segundos');
 
 function cronometro() {
-    const cuentaRegresivaDatos = new Date('11/08/2024').getTime(); // Fecha de fin
+    const cuentaRegresivaDatos = new Date('11/08/2024 22:00').getTime(); // Fecha de fin
 
     // Conversiones a milisegundos
     const segundosMilisegundos = 1000;
